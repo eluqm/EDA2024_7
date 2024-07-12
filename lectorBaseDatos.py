ecdf5929262cc6cf604b35a663644e26562d09f4
BryanLarico <blaricor@unsa.edu.pe>
2024-07-11 20:24:01 -0500

    Solucion1: para solucionar la lectura de base de datos se usa el utf-8, como se usa en html

--- lectorBaseDatos.py
+++ lectorBaseDatos.py
@@ -1,6 +1,6 @@
 import csv
 
-archivo_csv = open('BaseDatos/spotify_data.csv')
+archivo_csv = open('BaseDatos/spotify_data.csv', encoding='utf-8')
 archivo = csv.reader(archivo_csv, delimiter = ',')
 
 for fila in archivo:
