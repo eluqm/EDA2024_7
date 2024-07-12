ad88dcbafe4891c9febfde4dd972e4228ea08b5a
BryanLarico <blaricor@unsa.edu.pe>
2024-07-11 22:42:14 -0500

    Verificar Canciones: se esta comprobando si existen canciones en la base de datos (.csv) con mas de una hora de duracion

--- lectorBaseDatos.py
+++ lectorBaseDatos.py
@@ -3,6 +3,9 @@ import csv
 archivo_csv = open('BaseDatos/spotify_data.csv', encoding='utf-8')
 archivo = csv.reader(archivo_csv, delimiter = ',')
 
+next(archivo)
+
 for fila in archivo:
-    print(fila)
+    if (int(fila[18])) > 3599000:
+        print(fila[18])
 print('Fin de archivo')
