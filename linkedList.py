c7c0d0b52fc720be7d55d14cbc00fb9b39dce093
Eduardo <eportugalpor@unsa.edu.pe>
2024-07-14 14:27:22 -0500

    Mejorando el metodo add_song y agregando el metodo remove_song a la clase LinkedList

--- linkedList.py
+++ linkedList.py
@@ -15,8 +15,29 @@ class LinkedList:
         if not self.head:
             self.head = new_node
             self.tail = new_node
+            self.head.prev = self.head
+            self.head.next = self.head
         else:
             self.tail.next = new_node
-            new_node.prev = tail
-            tail = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+            self.head.prev = self.tail
+            self.tail.next = self.head
         self.size += 1
+    
+    def remove_song(self, song):
+        if self.head:
+            current = self.head
+            for _ in range(self.size):
+                if current.song == song:
+                    if self.size == 1:
+                        self.head = None
+                    else:
+                        current.prev.next = current.next
+                        current.next.prev = current.prev
+                        if current == self.head:
+                            self.head = current.next
+                    self.size -= 1
+                    return True
+                current = current.next
+        return False
