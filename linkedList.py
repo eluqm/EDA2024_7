c3797990fe0b8618060103c1fb93d86eecc15f77
Eduardo <eportugalpor@unsa.edu.pe>
2024-07-14 14:00:37 -0500

    Agregando el metodo add_song a la clase LinkedList

--- linkedList.py
+++ linkedList.py
@@ -7,4 +7,16 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = None
+        self.tail = None
         self.size = 0
+    
+    def add_song(self, song):
+        new_node = Node(song)
+        if not self.head:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev = tail
+            tail = new_node
+        self.size += 1
