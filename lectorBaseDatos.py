8e23d6c8670eaee0f7a71ab08c4a98b5c5aff332
BryanLarico <blaricor@unsa.edu.pe>
2024-07-14 13:11:54 -0500

    lectorBaseDatos.py: ahora se puede almacenar cada cancion como un objeto Song

--- lectorBaseDatos.py
+++ lectorBaseDatos.py
@@ -1,11 +1,15 @@
 import csv
 
+from trie import Trie
+from song import Song
+
 archivo_csv = open('BaseDatos/spotify_data.csv', encoding='utf-8')
 archivo = csv.reader(archivo_csv, delimiter = ',')
 
 next(archivo)
-
+trie = Trie()
 for fila in archivo:
-    if (int(fila[18])) > 3599000:
-        print(fila[18])
+    song = Song(fila[3], fila[2], fila[1], fila[6], fila[5], fila[4], fila[18])
+    print(song)
+    # print(fila[0])
 print('Fin de archivo')
