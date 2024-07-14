05e6da5f8592bf73f889837591cec43bfc820426
BryanLarico <blaricor@unsa.edu.pe>
2024-07-14 16:47:15 -0500

    lectorBaseDatos.py: creando archivo.bin

--- lectorBaseDatos.py
+++ lectorBaseDatos.py
@@ -1,7 +1,9 @@
 import csv
-
+import sys
 from trie import Trie
 from song import Song
+
+sys.setrecursionlimit(10000)
 
 archivo_csv = open('BaseDatos/spotify_data.csv', encoding='utf-8')
 archivo = csv.reader(archivo_csv, delimiter = ',')
@@ -10,6 +12,9 @@ next(archivo)
 trie = Trie()
 for fila in archivo:
     song = Song(fila[3], fila[2], fila[1], fila[6], fila[5], fila[4], fila[18])
-    print(song)
-    # print(fila[0])
+    trie.insert(song.getSong_name(), song)
+    # print(song)
 print('Fin de archivo')
+
+trie.saveFile('BaseDatos/trieSongName.bin')    
+print('Archivo creado')
