1cb875f79549921f89b42baaa2e3aa631b66ad5c
BryanLarico <blaricor@unsa.edu.pe>
2024-07-14 11:13:02 -0500

    trie.py: experimento con Clase Song

--- trie.py
+++ trie.py
@@ -1,3 +1,5 @@
+from song import Song
+
 class TrieNode:
     def __init__(self):
         self.children = {}
@@ -22,12 +24,21 @@ class Trie:
         node = self.root
         for char in word:
             if char not in node.children:
-                return False, []
+                return False
             node = node.children[char]
-        if node.endWord:
-            return True, node.data
-        else:
-            return False, []
+        return node.endWord
+            
+    def getSong(self, word):
+        if self.search(word):
+            node = self.root
+            for char in word:
+                node = node.children[char]
+            if node.endWord:
+                dats = ""
+                for dat in node.data:
+                    dats += f"{dat.getSong_name()}, {dat.getAuthor()}, {dat.getYear()}, {dat.getDuration()}\n"
+                return dats
+        return "No se encontraron resultados"
         
     def contains(self, prefix):
         node = self.root
@@ -37,10 +48,16 @@ class Trie:
             node = node.children[char]
         return True
         
-trie = Trie()
-trie.insert("hello", 12)
-trie.insert("hello", 23)
-trie.insert("hey", 1)
+cancion = Song("53QF56cjZA9RTuuMZDrSA6","I Won't Give Up","Jason Mraz", "acoustic", 2012, 68, 240166)
+print(cancion)
+cancion1 = Song("53QF56cjZA9RTuuMZDrS44","I Won't Give Up","Michael Jackson", "pop", 2012, 68, 3725000)
+print(cancion1)
+cancion2 = Song("ghtF56cjZA9RTuuMZDrSA6","Its my live","Bon Jovi", "rock", 2002, 68, 2725000)
+print(cancion2)
 
-print(trie.search("hello"))
-print(trie.contains("he"))
+rep = Trie()
+rep.insert(cancion.getSong_name(), cancion)
+rep.insert(cancion1.getSong_name(), cancion1)
+rep.insert(cancion2.getSong_name(), cancion2)
+
+print(rep.getSong("I Won't Give Up"))
