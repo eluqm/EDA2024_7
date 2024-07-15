53604e456df51aab7cc59b57f5504849e0c24a2b
BryanLarico <blaricor@unsa.edu.pe>
2024-07-15 13:22:38 -0500

    lectorBaseDatos.py: ahora funciona una tabla donde se muestran las coincidencias de nombres de canciones (mostrando los atributos del objeto song)

--- lectorBaseDatos.py
+++ lectorBaseDatos.py
@@ -3,7 +3,10 @@ import sys
 from trie import Trie
 from song import Song
 import customtkinter as ctk
+from tkinter import ttk
 import tkinter as tk
+
+
 
 archivo_csv = open('BaseDatos/spotify_data.csv', encoding='utf-8')
 archivo = csv.reader(archivo_csv, delimiter = ',')
@@ -15,23 +18,57 @@ for fila in archivo:
     trie.insert(song.getSong_name(), song)
     # print(song)
 print('Fin de archivo')
+"""
+cancion = Song("53QF56cjZA9RTuuMZDrSA6","Hello","Jason Mraz", "acoustic", 2012, 68, 240166)
+print(cancion)
+cancion1 = Song("53QF56cjZA9RTuuMZDrS44","Hello","Michael Jackson", "pop", 2012, 68, 3725000)
+print(cancion1)
+cancion2 = Song("ghtF56cjZA9RTuuMZDrSA6","Its my live","Bon Jovi", "rock", 2002, 68, 2725000)
+print(cancion2)
+
+trie = Trie()
+trie.insert(cancion.getSong_name(), cancion)
+trie.insert(cancion1.getSong_name(), cancion1)
+trie.insert(cancion2.getSong_name(), cancion2)
+"""
 
 ctk.set_appearance_mode("System")
 ctk.set_default_color_theme("blue")
 
 app = ctk.CTk()
-app.geometry("400x240")
+app.geometry("400x440")
 
 def getText():
-    text = textbox.get("1.0", "end-1c")
-    print(trie.getSong(text))
-    
-ctk.CTkLabel(app, text="Nombre de cancion").pack()
-    
-textbox = ctk.CTkTextbox(app, width=200, height=50)
-textbox.place(relx=0.5, rely=0.2, anchor=tk.CENTER)
+    for item in tree.get_children():
+        tree.delete(item)
 
-button = ctk.CTkButton(master=app, text="Buscar", command=getText)
-button.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+    songName = textbox.get("1.0", "end-1c")
+    
+    arrSongs = trie.getSong(songName)
+    for song in arrSongs:
+        tree.insert("", "end", values=(song.getSong_name(), song.getAuthor(), song.getGenre(), song.getYear(), song.getDuration()))
+
+main = ctk.CTkFrame(app)
+main.pack(fill="both", expand=True, padx=10, pady=10)
+
+ctk.CTkLabel(main, text="Nombre de cancion").pack(pady=10)
+    
+textbox = ctk.CTkTextbox(main, width=200, height=50)
+textbox.pack(pady=10)
+
+button = ctk.CTkButton(main, text="Buscar", command=getText)
+button.pack(pady=10)
+
+table = ctk.CTkFrame(main)
+table.pack(pady=10, fill="both", expand=True)
+columns = ("Nombre", "Autor", "Género", "Año", "Duración")
+tree = ttk.Treeview(table, columns=columns, show="headings", height=8)
+for col in columns:
+    tree.heading(col, text=col)
+    tree.column(col, width=80, anchor="center")
+scroll_y = ttk.Scrollbar(table, orient="vertical", command=tree.yview)
+scroll_y.pack(side="right", fill="y")
+tree.configure(yscroll=scroll_y.set)
+tree.pack(fill="x", expand=False)
 
 app.mainloop()
