b9eea1e0cb4f477fecac90cbd3605b5ef28b2811
Eduardo <eportugalpor@unsa.edu.pe>
2024-07-16 18:50:00 -0500

    Agregando elementos para mostrar el nombre, autor, y numero de vistas de la cancion

--- playlist_interface.py
+++ playlist_interface.py
@@ -19,6 +19,13 @@ song_item.pack(pady=1)
 icon_song_data = Image.open("src/default-icon.jpeg")
 icon_song = CTkImage(dark_image=icon_song_data, light_image=icon_song_data, size=(40, 40))
 CTkLabel(master=song_item, text="", image=icon_song).pack(side="left", pady=(5, 5), padx=(5, 5))
+# Datos de la cancion
+specifications = CTkFrame(master=song_item, width=210, height=50, fg_color="#000000", corner_radius=0)
+specifications.pack(side="left", padx=(0, 0))
+song_name = CTkLabel(master=specifications, text="Nombre de la cancion", text_color="#ffffff", font=("Arial Bold", 15), width=210, height=30, anchor="w")
+song_name.pack(pady=(0, 0))
+song_data = CTkLabel(master=specifications, text="Autor · N° vistas", text_color="#ffffff", font=("Arial Bold", 10), width=210, height=20, anchor="w")
+song_data.pack(pady=(0, 0))
 
 
 window.mainloop()
