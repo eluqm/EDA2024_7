ef985731664c17ce10458d733ecbf23fff848a6a
Eduardo <eportugalpor@unsa.edu.pe>
2024-07-16 18:40:00 -0500

    Comenzando a crear el elemento de una cancion en la lista, agregando la imagen a la izquierda

--- playlist_interface.py
+++ playlist_interface.py
@@ -12,4 +12,13 @@ window.configure(fg_color='black')
 playlist = CTkScrollableFrame(master=window, width=380, height=450, fg_color="#272727", corner_radius=0,)
 playlist.pack(expand=True, side="right", pady=(30, 20), padx=(0, 20))
 
+song_item = CTkFrame(master=playlist, width=380, height=50, fg_color="#000000", corner_radius=0)
+song_item.pack(pady=1)
+
+# Imagen
+icon_song_data = Image.open("src/default-icon.jpeg")
+icon_song = CTkImage(dark_image=icon_song_data, light_image=icon_song_data, size=(40, 40))
+CTkLabel(master=song_item, text="", image=icon_song).pack(side="left", pady=(5, 5), padx=(5, 5))
+
+
 window.mainloop()
