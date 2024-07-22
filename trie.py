d1261ae74365f8031de25015e27ab80f4429cfaf
BryanLarico <blaricor@unsa.edu.pe>
2024-07-22 16:57:35 -0500

    trie: metodo getSongByYear() creado para implementar en la interfaz

--- trie.py
+++ trie.py
@@ -41,9 +41,9 @@ class Trie:
                     dats.append(dat)
                 return dats
         return []
-    # Tiempo O(n)
+    # Tiempo no eficiente
     
-    def getSong(self, songName, author):
+    def getSongByAuthor(self, songName, author):
         if self.search(songName):
             node = self.root
             for char in songName:
@@ -53,6 +53,18 @@ class Trie:
                     if dat.author == author:
                         return dat
         return
+        
+    def getSongByYear(self, songName, year):
+        data = []
+        if self.search(songName):
+            node = self.root
+            for char in songName:
+                node = node.children[char]
+            if node.endWord:
+                for dat in node.data:
+                    if dat.year == year:
+                        data.append(dat)
+        return data
         
     def contains(self, prefix):
         node = self.root
@@ -70,7 +82,7 @@ class Trie:
         with open(file, 'rb') as f:
             return pickle.load(f)
 
-"""    
+""
 cancion = Song("53QF56cjZA9RTuuMZDrSA6","I Won't Give Up","Jason Mraz", "acoustic", 2012, 68, 240166)
 print(cancion)
 cancion1 = Song("53QF56cjZA9RTuuMZDrS44","I Won't Give Up","Michael Jackson", "pop", 2012, 68, 3725000)
@@ -83,5 +95,7 @@ rep.insert(cancion.getSong_name(), cancion)
 rep.insert(cancion1.getSong_name(), cancion1)
 rep.insert(cancion2.getSong_name(), cancion2)
 
-print(rep.getSong("I Won't Give Up"))
-"""
+print("Busqueda por nombre y año")
+canciones = rep.getSongByYear("I Won't Give Up",2012)
+for cancion in canciones:
+    print(cancion)
